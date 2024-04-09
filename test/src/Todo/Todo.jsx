1c01826846dd7799f1d16b5f5b3926cabc2a9272
Himanshuakodiya19@gmail.com <himanshuakodiya19@gmail.com>
2024-04-10 01:26:59 +0530

    change function

--- test/src/Todo/Todo.jsx
+++ test/src/Todo/Todo.jsx
@@ -4,9 +4,10 @@ import TodoStyle from "./Style.module.css";
 function Todo() {
   const [UserInput, setUserInput] = useState("");
 
-  function handleValue(e) {
-    setUserInput(e.target.value);
-  }
+  const [complete,setcomplete] = useState(0);
+  const[remaining,setremaining]= useState(0);
+
+  
 
  const AllData = [
    { task:"Buy Car",completed:true}, 
@@ -17,9 +18,13 @@ function Todo() {
     ]
   const [todo, setTodo] = useState(AllData);
 
+  function handleValue(e) {
+    setUserInput(e.target.value);
+  }
+
   function handleTask() {
     if(UserInput){
-    setTodo([...todo,{ task: UserInput, completed:false}]);
+    setTodo([...todo,{  UserInput, completed:false}]);
     setUserInput(" ");
   }
 }
@@ -27,16 +32,45 @@ function Todo() {
 function handleCheck(index){
   const MyCheckArray =[...todo]
   MyCheckArray[index].completed = !MyCheckArray[index].completed
-  console.log(MyCheckArray)
   setTodo(MyCheckArray)
 
+  let completedTask =  MyCheckArray.filter((value,index)=>{
+   return value.completed   
+  })
+  let remainingTask = MyCheckArray.filter((value,index)=>{
+    return ! value.completed  
+  })
+  setcomplete(completedTask.length)
+  setremaining(remainingTask.length)
 }
+
+function handleDelete(index){
+
+  let MyArray = [...todo]
+
+ let deleteItems=  MyArray.filter((value,id)=>{
+    return index !== id
+
+  })
+
+  setTodo(deleteItems)
+}
+
+function handleUpdate(index){
+  let updateArray = [...todo]
+ let getValue =updateArray[index].task
+let updatedValue = prompt(`Update Your task :- ${getValue}` , getValue)
+let newValue = { tesk:updatedValue,complete:false}
+updateArray.splice(index,0,newValue)
+setTodo(updateArray)
+}
+
 
   return (
     <>
       <div className={TodoStyle.todo}>
         <div className={TodoStyle.todo_main}>
-          <h1 className={TodoStyle.h1}>My Todo App</h1>
+          <h1 className={TodoStyle.h1}>My Todo App 📃</h1>
           <input
             type="text"
             placeholder="Enter Task"
@@ -49,17 +83,32 @@ function handleCheck(index){
           </button>
 
           {todo.map((value, index) => (
-            <ul className={TodoStyle.list}>
+            <div className={TodoStyle.list}>
               <input type="checkbox" 
               checked ={value.completed}
               onClick={()=>{handleCheck(index)}}
               />
-           
+            
               <span style={{textDecoration:value.completed ? "line-through" : " "}} 
+  >{value.task}</span>
 
-              >{value.task}</span>
-            </ul>
+<span className={TodoStyle.icon}>
+              <i className={"bi bi-x-circle-fill"}
+              onClick={()=>{
+           handleDelete(index) }}></i>
+              </span>
+
+              <span className={TodoStyle.icon_edit}>
+              <i className={"bi bi-pencil-square"}
+              onClick={()=>{
+                handleUpdate(index)
+              }}></i>
+              </span>
+
+            </div> 
           ))}
+          <span>Completed :- {complete}</span>
+           <span>Remaining :- {remaining}</span>
         </div>
       </div>
     </>
